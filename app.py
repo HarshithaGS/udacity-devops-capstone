638bdbd43f6a35bfdf580e4ce6b0f4ef3e144990
HarshithaGS <harshithags6595@gmail.com>
2021-12-29 22:46:42 +0000

    Initial Commit

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app= Flask(__name__)
 #Homepage
 @app.route('/')
 def hello():
-    return "<h2>Hello, Welcome to Harshithas Capstone Project.<h2>"
+    return "Hello, Welcome to Harshithas Capstone Project."
     
 #run on port 80
-if __name__=='__main__':
+if __name__=="__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
+    
+    
+    
+   
